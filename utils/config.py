a63772609fb0091e01212b3db949c222a3860199
zw <2283499971@qq.com>
2017-04-19 21:54:56 +0800

    test ignore passwd

--- utils/config.py
+++ utils/config.py
@@ -3,12 +3,6 @@ import yaml
 import sys,os
 
 def get_home_dir():
-    # f = open('../utils/config.yaml')
-    # f = open('../utils/config.yaml')
-    # f = open('/home/zw/Documents/project/tianchi/utils/config.yaml')
-    # x = yaml.load(f)
-    # f.close()
-    # return x['HOME_DIR']
     return os.path.dirname(os.getcwd())
 
 def get_title_weight():
